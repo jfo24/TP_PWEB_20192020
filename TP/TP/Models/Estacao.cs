1520b70f1bf13e66bab296143b9f690029eb410f
Diogo <diogomarante23@gmail.com>
2019-11-17 23:32:25 +0000

    Adição do modelo Estacao

---
+++ TP/TP/Models/Estacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP.Models
+{
+    public class Estacao
+    {
+        public int EstacaoID { get; set; }
+
+        public string Localizacao { get; set; }
+
+        public int NumVeiculos { get; set; }
+
+        public TipoVeiculo Tipo { get; set; }
+    }
+}
