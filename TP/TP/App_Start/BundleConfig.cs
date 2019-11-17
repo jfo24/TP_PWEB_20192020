c77d4af0ab144dc5cd8b3f1f922b26ac7daccddb
João Oliveira <joao_oliveira_1997@hotmail.com>
2019-11-17 18:34:21 +0000

    Menu Incompleto

--- TP/TP/App_Start/BundleConfig.cs
+++ TP/TP/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace TP
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-custom.css",
                       "~/Content/site.css"));
         }
     }
--- TP/TP/Models/utilizador.cs
+++ TP/TP/Models/utilizador.cs
@@ -7,13 +7,13 @@ namespace TP.Models
 {
     public class Utilizador
     {
-        public int UtilizadorID { get; set; }
+        public int ID { get; set; }
 
         public string Nome { get; set; }
 
         public string Morada { get; set; }
 
-        public string UserEmail { get; set; }
+        public string Email { get; set; }
 
         public int NIB { get; set; }
     }
