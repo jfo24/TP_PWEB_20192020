0b97240d73ea6e14cbadb32ed3e8dea4afc75558
Diogo <diogomarante23@gmail.com>
2019-11-28 20:02:57 +0000

    About completo e Registo do Cliente

---
+++ TP/TP/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TP.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult login()
+        {
+            return View();
+        }
+
+        public ActionResult registo()
+        {
+            return View();
+        }
+    }
+}
