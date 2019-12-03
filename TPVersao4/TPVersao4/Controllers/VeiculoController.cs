fc8e77a23d874b907e758e2a5bb18d07acd68c0c
João Oliveira <joao_oliveira_1997@hotmail.com>
2019-12-03 16:01:55 +0000

    Login e Controlador Veiculo

---
+++ TPVersao4/TPVersao4/Controllers/VeiculoController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TPVersao4.Models;
+
+namespace TPVersao4.Controllers
+{
+    public class VeiculoController : Controller
+    {
+        private GreenChargeContext db = new GreenChargeContext();
+
+        // GET: Veiculo
+        public ActionResult Index()
+        {
+            return View(db.Veiculos.ToList());
+        }
+
+        // GET: Veiculo/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Veiculo veiculo = db.Veiculos.Find(id);
+            if (veiculo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(veiculo);
+        }
+
+        // GET: Veiculo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Veiculo/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "VeiculoID,TipoVeiculo,VeiculoPreco,VeiculoBateria")] Veiculo veiculo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Veiculos.Add(veiculo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(veiculo);
+        }
+
+        // GET: Veiculo/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Veiculo veiculo = db.Veiculos.Find(id);
+            if (veiculo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(veiculo);
+        }
+
+        // POST: Veiculo/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "VeiculoID,TipoVeiculo,VeiculoPreco,VeiculoBateria")] Veiculo veiculo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(veiculo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(veiculo);
+        }
+
+        // GET: Veiculo/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Veiculo veiculo = db.Veiculos.Find(id);
+            if (veiculo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(veiculo);
+        }
+
+        // POST: Veiculo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Veiculo veiculo = db.Veiculos.Find(id);
+            db.Veiculos.Remove(veiculo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
